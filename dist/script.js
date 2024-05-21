9575117ebc86ff9c95a9886a7bfdc746e531a219
shivam-official-26 <shivamofficial2626@gmail.com>
2024-05-21 19:24:55 +0530

    Complete UI

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,4 @@
-console.log(`hello`);
+const startNewGameBtn = document.querySelector("#startNewGame");
+startNewGameBtn.addEventListener("click", () => {
+  window.open("./game.html","_self");
+});
